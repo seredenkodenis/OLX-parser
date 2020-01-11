21390dd7e43edf007d4dd71feaac4dcc739bab3d
denis <denisvitalievich5@gmail.com>
2020-01-11 22:33:16 +0200

    Easy install and function with many pages

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,26 @@
 import java.io.IOException;
-
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) throws IOException {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Welcome to OLX-parser!");
+        System.out.println("Created by Denys Seredenko");
+        System.out.println("----------------------------");
+        System.out.println("Please input your CORRECT url address");
+        String url = in.nextLine();
+        System.out.println("Claim! Write how many pages do you want to parse?(Type a count)");
+        System.out.println("We recommend you to write < 5");
+        Integer pages = in.nextInt();
+        System.out.println("Claim! Now choose your option");
       Victim rozetka = new Victim();
-      //rozetka.setParametr(".price");
-      rozetka.setWebUrl("https://www.olx.ua/list/q-%D0%BC%D1%8B%D1%88%D0%BA%D0%B0/?search%5Bprivate_business%5D=private");
-      rozetka.checkUrl();
-      rozetka.sitePrise();
-      rozetka.getNames();
-      //Victim ali  = new Victim("https://www.amazon.com/-/de/dp/B0742HZW9B/ref=lp_18332383011_1_1?srs=18332383011&ie=UTF8&qid=1578690362&sr=8-1");
-      //ali.checkUrl();
+      rozetka.setWebUrl(url);
+      for(int j = 0; j < pages; ++j){
+              rozetka.setWebUrl(url + "?page=" + j+1);
+          rozetka.checkUrl();
+          rozetka.sitePrise();
+          rozetka.getNames();
+          rozetka.getUrls();
+      }
     }
 }
--- src/main/java/Victim.java
+++ src/main/java/Victim.java
@@ -2,18 +2,21 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.Buffer;
 
 public class Victim {
     private String webUrl;
     private String priceParametr =".price > strong";
-    private String urlParametr = ".rel > h3 > a > strong";
+    private String nameParametr = ".rel > h3 > a > strong";
+    private String urlParametr = ".rel > h3 > a[href]";
     private int[] pricesInt = new int[1000];
     private String[] names = new String[1000];
+    private String[] urls = new String[1000];
     private Document document;
+    int col  = 1;
 
     //Method for checking if url exists
     public void checkUrl() throws IOException {
@@ -29,6 +32,8 @@ public class Victim {
             Elements elements = document.select(priceParametr);
             for(Element element: elements){
                 ++j;
+                if(element.ownText().equals("Обмен")) continue;
+                if(element.ownText().equals("Бесплатно")) continue;
                 prices[j] = element.ownText();
                 prices[j] = prices[j].replaceAll(" ","");
                 prices[j] = prices[j].replaceAll("грн.","");
@@ -48,7 +53,7 @@ public class Victim {
     //Method for prices save
     public void savePrices(int[] pricesInt){
         try {
-            BufferedWriter writer = new BufferedWriter(new FileWriter("prices"));
+            BufferedWriter writer = new BufferedWriter(new FileWriter(String.valueOf(col) + "_prices"));
             for(int i = 0; i < pricesInt.length; ++i){
                 if(pricesInt[i] == 0){
                     break;
@@ -64,7 +69,7 @@ public class Victim {
 
     //Method for parsing titles
     public void getNames(){
-        Elements elements = document.select(urlParametr);
+        Elements elements = document.select(nameParametr);
         int i = -1;
         for(Element element: elements){
             i++;
@@ -76,13 +81,38 @@ public class Victim {
     //Method for saving titles
     public void saveNames(String[] names){
         try {
-            BufferedWriter writer = new BufferedWriter(new FileWriter("names"));
+            BufferedWriter writer = new BufferedWriter(new FileWriter(String.valueOf(col) + "_names"));
             for(int i = 0; i < names.length; ++i){
                 if(names[i] == null) break;
                 writer.write(i+1 + ") " + names[i]);
                 writer.newLine();
             }
             writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void getUrls(){
+        Elements elements = document.select(urlParametr);
+        int j = -1;
+        for(Element element: elements){
+            j++;
+            urls[j] = String.format("%s", element.attr("abs:href"),element.text());
+        }
+        saveUrls(urls);
+        col++;
+    }
+
+    public void saveUrls(String[] urls){
+        try {
+            BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(String.valueOf(col) + "_urls"));
+            for(int i = 0; i < urls.length; ++i){
+                if(urls[i] == null) break;
+                bufferedWriter.write(i+1 + ") " + urls[i]);
+                bufferedWriter.newLine();
+            }
+            bufferedWriter.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
